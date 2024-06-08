9beac2b4651cf46939358bdd890aadd41d97c4ed
Kapil Gwal <2909kapil2001@gmail.com>
2024-06-08 13:23:31 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,36 @@
+import streamlit as st
+import pickle
+import numpy as np
+
+pipe = pickle.load(open('..NaiveBayes.pkl','rb'))
+df = pickle.load(open('..NaiveBayes.pkl','rb'))
+
+st.title("Crop Suggestion System")
+
+# N
+nitrogen = st.number_input('Nitrogen')
+
+# P
+phosphorus = st.number_input('Phosphorus')
+
+# K
+potassium = st.number_input('Potassium')
+
+# 'tempreature'
+tempreature = st.number_input('tempreature in celcius')
+
+# humudity
+humidity = st.number_input('humidity')
+
+# ph
+ph = st.number_input('ph')
+
+# rainfall
+rainfall = st.number_input('rainfall in mm')
+
+if st.button("Predict"):
+    query = np.array([[nitrogen,phosphorus,potassium,tempreature,humidity,ph,rainfall]])
+    # print(query)
+    text = str(pipe.predict(query)[0])
+    result = text
+    st.title("The crop suggested for this demographic condition is : " + result)
